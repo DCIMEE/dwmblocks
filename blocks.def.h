64c87dccb8af52f67dd252655db995e6769469a4
DCIMEE <backslash74@outlook.com>
2025-01-02 19:49:52 +1300

    config update

--- blocks.def.h
+++ blocks.def.h
@@ -4,17 +4,17 @@ static const Block blocks[] = {
 
 	//{"󰂄 ", "cat /sys/class/power_supply/BAT1/capacity",					300,		0},
 
-	{"", "~/suckless/dwmblocks/scripts/dwm_battery.sh",					300,		0},
+	{"", "~/suckless/dwmblocks/scripts/dwm_battery.sh",					300,		1},
 
-	{"  ", "amixer get Master | awk -F'[][]' '/Left:/ {print $2}'",					1,		0},
+	{"  ", "amixer get Master | awk -F'[][]' '/Left:/ {print $2}'",					1,		5},
 
-	{"", "~/suckless/dwmblocks/scripts/dwm_weather.sh",					1800,		0},
+	{"", "~/suckless/dwmblocks/scripts/dwm_weather.sh",					1800,		2},
 
-	{"", "~/suckless/dwmblocks/scripts/wlnet",					5,		0},
+	{"", "~/suckless/dwmblocks/scripts/wlnet",					5,		3},
 
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "date '+%b %d (%a) %I:%M%p'",					5,		4},
 
 };
 
